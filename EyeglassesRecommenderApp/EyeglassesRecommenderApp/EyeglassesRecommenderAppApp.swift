10d4e657ac869914c1e09911b530c7d3a1123bd9
daniel-bss <daniel.simamora2000@gmail.com>
2023-08-21 16:35:50 +0700

    setup xcode project & gitignore

---
+++ EyeglassesRecommenderApp/EyeglassesRecommenderApp/EyeglassesRecommenderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeglassesRecommenderAppApp.swift
+//  EyeglassesRecommenderApp
+//
+//  Created by Daniel Bernard Sahala Simamora on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct EyeglassesRecommenderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

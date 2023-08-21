20b4a2650526b23984fa623149cc33d765ded1c0
Anisya Dewi Larasati <anisyadewilarasati@gmail.com>
2023-08-21 22:04:50 +0700

    Initial Commit

---
+++ FrameGlasses/FrameGlassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameGlassesApp.swift
+//  FrameGlasses
+//
+//  Created by Anisya Dewi Larasati on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FrameGlassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
